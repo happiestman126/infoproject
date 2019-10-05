8601539c5c838d84643f31d143438876fd1d589d
unknown <somusunilkumar@gmail.com>
2019-10-05 23:28:42 +0530

    2nd version

--- app.module.ts
+++ app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+
+import {HttpModule} from '@angular/http';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WebICComponent } from './web-ic/web-ic.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const mypage : Routes = [
+  {path:"LoginComponent", component:LoginComponent},
+  {path:"HomeComponent", component:HomeComponent}
+                          ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebICComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule, 
+    RouterModule,
+    HttpModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
